92d32ed9bd6e33b8ed8a21dd8e25e7fe67a7e7dc
KaiOWhatmore <oscar1995@live.com.au>
2023-12-15 14:36:38 +0800

    change ui colours and include cloudfare font awesome

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import Navbar from './components/navigations/Navbar';
 import Footer from './components/navigations/Footer'
 import { Routes, Route } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route path="/home" element={<div><h1>Home Page</h1><p>Welcome to the home page!</p></div>} />
-        <Route path="/catalog" element={<div><h1>Catalog Us</h1><p>Learn more about us here.</p></div>} />
-        <Route path="/contact" element={<div><h1>Contact Us</h1><p>Get in touch with us.</p></div>} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/home" element={<div><h1>Home Page</h1><p>Welcome to the home page!</p></div>} />
+          <Route path="/catalog" element={<div><h1>Catalog Us</h1><p>Learn more about us here.</p></div>} />
+          <Route path="/contact" element={<div><h1>Contact Us</h1><p>Get in touch with us.</p></div>} />
+        </Routes>
+      </main>
       <Footer />
     </div>
   );
