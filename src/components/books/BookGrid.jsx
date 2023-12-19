f02def4f02b09138bbb22782302585e28765c115
KaiOWhatmore <oscar1995@live.com.au>
2023-12-19 13:31:07 +0800

    include csv for testing on book list

--- src/components/books/BookGrid.jsx
+++ src/components/books/BookGrid.jsx
@@ -1,14 +1,39 @@
 // components/BookGrid.js
-import React from 'react';
-import Book from './Book';
-import booksDummyData from './booksDummyData';
+import React from "react";
+import Book from "./Book";
+import booksDummyData from "./booksDummyData";
+import Papa from "papaparse";
+import Book from "./Book";
 
-const BookGrid = () => (
-  <div className="book-grid">
-    {booksDummyData.map((book, index) => (
-      <Book key={index} {...book} />
-    ))}
-  </div>
-);
+// Custom hook for fetching book data
+const useBookData = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/output-sorted-read.csv") // Adjust the path if necessary
+      .then((response) => response.text())
+      .then((csvString) => {
+        const parsedData = Papa.parse(csvString, {
+          header: true,
+          skipEmptyLines: true,
+        }).data;
+        setData(parsedData);
+      });
+  }, []);
+
+  return data;
+};
+
+const BookGrid = () => {
+  const books = useBookData();
+
+  return (
+    <div className="book-grid">
+      {books.map((book, index) => (
+        <Book key={index} {...book} />
+      ))}
+    </div>
+  );
+};
 
 export default BookGrid;
---
+++ src/components/books/BookParser.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import Papa from 'papaparse';
+
+const BookParser = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('../../../public/output-sorted-read.csv')
+      .then(response => response.text())
+      .then(csvString => {
+        const parsedData = Papa.parse(csvString, {
+          header: true,
+          skipEmptyLines: true,
+        }).data;
+        setData(parsedData);
+      });
+  }, []);
+
+  // Rest of your component logic...
+};
+
+export default BookParser;
