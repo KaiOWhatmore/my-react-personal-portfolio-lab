a6526d9a250a804678998bee9f656910d7cc6f4c
KaiOWhatmore <oscar1995@live.com.au>
2023-12-21 23:01:31 +0800

    change css so that the terminal uses stacked awesome fonts

--- src/components/pages/WhatIDo.jsx
+++ src/components/pages/WhatIDo.jsx
@@ -1,38 +1,61 @@
-import React from 'react';
-import './WhatIDo.css'; // Make sure to create this CSS file and import it here
+import React from "react";
+import "./WhatIDo.css"; // Make sure to create this CSS file and import it here
 
 function WhatIDo() {
-    return (
-        <div className="what-i-do-container">
-            <h1 className='header'>What I Do</h1>
-            <p className="subtitle">SELF TAUGHT DEVELOPER WHO IS TOO CURIOUS FOR HIS OWN GOOD</p>
-            <div className="icons">
-                <i className="fab fa-java custom-java"></i>
-                <i className="fab fa-python custom-python"></i>
-                <i className="fas fa-database custom-database"></i>
-                <i className="fas fa-leaf custom-spring"></i>
-                <i className="fas fa-feather custom-maven"></i>
-                <i className="fab fa-docker custom-docker"></i>
-                <i className="fab fa-aws custom-aws"></i>
-                <i className="fas fa-terminal custom-terminal"></i>
-            </div>
-            <ul className='professional-spiel'>
-                <li>Working in enterprise environments working with complex backend systems</li>
-                <li>Putting my nose to the grindwheel on REST APIs, Back Office backend engineering, DevOps and Developer Tools</li>
-                <li>Self teaching React and FrontEnd development for pet projects and professional aspirations</li>
-            </ul>
-            <hr className='line-separator' />
-            <div>
-                <ul className='other-interests'>
-                    <p>Phew.  Now that the professional spiel is over, my other interests include</p>
-                    <li> Reading.  And I mean A LOT.  See my goodreads [insert link here] </li>
-                    <li> Photography and film go to my youtube channel here [insert link here]</li>
-
-                </ul>
-            </div>
-
-        </div>
-    );
-};
+  return (
+    <div className="what-i-do-container">
+      <h1 className="header">What I Do</h1>
+      <p className="subtitle">
+        SELF TAUGHT DEVELOPER WHO IS TOO CURIOUS FOR HIS OWN GOOD
+      </p>
+      <div className="icons">
+        <i className="fab fa-java custom-java fa-2x"></i>
+        <i className="fab fa-python custom-python fa-2x"></i>
+        <i className="fas fa-database custom-database fa-2x"></i>
+        <i className="fas fa-leaf custom-spring fa-2x"></i>
+        <i className="fas fa-feather custom-maven fa-2x"></i>
+        <i className="fab fa-docker custom-docker fa-2x"></i>
+        <i className="fab fa-aws custom-aws fa-2x"></i>
+        {/* <i className="fas fa-terminal custom-terminal fa-2x"></i> */}
+        <span className="fas fa-stack custom-terminal fa-1x">
+          <i class="fas fa-solid fa-square fa-stack-2x"></i>
+          <i class="fas fa-solid fa-terminal fa-stack-1x fa-inverse"></i>
+        </span>
+      </div>
+      <ul className="professional-spiel">
+        <li>
+          Working in enterprise environments working with complex backend
+          systems
+        </li>
+        <li>
+          Putting my nose to the grindwheel on REST APIs, Back Office backend
+          engineering, DevOps and Developer Tools
+        </li>
+        <li>
+          Self teaching React and FrontEnd development for pet projects and
+          professional aspirations
+        </li>
+      </ul>
+      <hr className="line-separator" />
+      <div>
+        <ul className="other-interests">
+          <p>
+            Phew. Now that the professional spiel is over, my other interests
+            include
+          </p>
+          <li>
+            {" "}
+            Reading. And I mean A LOT. See my goodreads [insert link here]{" "}
+          </li>
+          <li>
+            {" "}
+            Photography and film go to my youtube channel here [insert link
+            here]
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 export default WhatIDo;
