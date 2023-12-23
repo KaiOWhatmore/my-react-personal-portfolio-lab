774cbac488c8ed83c029f2b2715e43cf153f1641
KaiOWhatmore <oscar1995@live.com.au>
2023-12-23 14:32:28 +0800

    WORKING: include functioning TimeLine with work experience

--- src/components/pages/TimeLine.jsx
+++ src/components/pages/TimeLine.jsx
@@ -4,8 +4,11 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import ParentComponent from "./ParentComponent.jsx";
-import "./TimeLine.css";
 import worldline from "../../images/worldline.png";
+import medirecords from "../../images/medirecords.webp";
+import usyd from "../../images/usyd.jpg";
+import btc from "../../images/btc-logo.png";
+import "./TimeLine.css";
 
 const techList = [
   "Java",
@@ -17,36 +20,47 @@ const techList = [
   "JBoss",
 ];
 
+const elementStyles = {
+  contentStyle: {
+    background: "transparent ",
+    // border: "2px solid rgb(33, 150, 243)", // Adjust border thickness as needed,
+    boxShadow: "0px 0px 10px rgba(33, 150, 243, 0.5)",
+    color: "black",
+    marginLeft: "70px",
+    // marginTop: "20px"
+  },
+  workIconStyle: {
+    background: "rgb(33, 150, 243)",
+    color: "#fff",
+    width: "70px",
+    height: "70px",
+    marginLeft: "-15px", // Half of the width to center the icon
+  },
+  arrowStyle: {
+    borderRight: "transparent",
+  },
+  imageStyle: {
+    width: "100%",
+    height: "100%",
+    borderRadius: "50%",
+  },
+};
+
 const TimeLine = () => {
   return (
     <div className="timeline-container">
       <VerticalTimeline lineColor={"black"} layout={"1-column-left"}>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          contentStyle={{
-            background: "transparent ",
-            // border: "2px solid rgb(33, 150, 243)", // Adjust border thickness as needed,
-            boxShadow: "0px 0px 10px rgba(33, 150, 243, 0.5)",
-            color: "#fff",
-            marginLeft: "70px",
-            // marginTop: "20px"
-          }}
-          contentArrowStyle={{
-            borderRight: "transparent",
-          }}
+          contentStyle={elementStyles.contentStyle}
+          contentArrowStyle={elementStyles.arrowStyle}
           date="2022-2023"
-          iconStyle={{
-            background: "rgb(33, 150, 243)",
-            color: "#fff",
-            width: "70px",
-            height: "70px",
-            marginLeft: "-15px", // Half of the width to center the icon
-          }}
+          iconStyle={elementStyles.workIconStyle}
           icon={
             <img
               src={worldline}
               alt="worldline"
-              style={{ width: "100%", height: "100%", borderRadius: "50%" }}
+              style={elementStyles.imageStyle}
             />
           }
         >
@@ -54,68 +68,104 @@ const TimeLine = () => {
           <h4 className="vertical-timeline-element-subtitle">
             Java Backend Developer
           </h4>
-          <p>Develoepr Tool Creator, Devops, Avante Gardner </p>
-          <ParentComponent technologies={techList} />
+          <p>
+            Batch Processing, Pipelines, Developer Tool Creator, Devops, Avante
+            Gardner{" "}
+          </p>
+          <ParentComponent
+            technologies={[
+              "Java",
+              "Spring",
+              "Maven",
+              "Git",
+              "Docker",
+              "Bash",
+              "Jenkins",
+              "PL/SQL",
+              "JBoss",
+            ]}
+          />
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2010 - 2011"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          contentStyle={elementStyles.contentStyle}
+          contentArrowStyle={elementStyles.arrowStyle}
+          date="2021-2022"
+          iconStyle={elementStyles.workIconStyle}
+          icon={
+            <img
+              src={medirecords}
+              alt="medirecords"
+              style={elementStyles.imageStyle}
+            />
+          }
         >
-          <h3 className="vertical-timeline-element-title">Art Director</h3>
           <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
+            <h3 className="vertical-timeline-element-title">MediRecords</h3>
+            Junior Java Backend Developer
+          </h4>
+          <p>FHIR REST APIs, Healthtech, DevOps, Support, ... </p>
+          <ParentComponent
+            technologies={[
+              "Java",
+              "Spring",
+              "Gradle",
+              "Jenkins",
+              "Git",
+              "Docker",
+              "PostgreSQL",
+              "FreeMarker",
+              "JQuery",
+            ]}
+          />
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={elementStyles.contentStyle}
+          contentArrowStyle={elementStyles.arrowStyle}
+          date="2020-2021"
+          iconStyle={elementStyles.workIconStyle}
+          icon={
+            <img
+              src={btc}
+              alt="break-the-chain"
+              style={elementStyles.imageStyle}
+            />
+          }
+        >
+          <h3 className="vertical-timeline-element-title">Break The Chain</h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Data Engineer, Analyst, Product Advisor
           </h4>
           <p>
-            Creative Direction, User Experience, Visual Design, SEO, Online
-            Marketing
+            ETL Pipelines, Financial Analysis, Quantitative Product Analysis
           </p>
+          <ParentComponent
+            technologies={[
+              "Python",
+              "FBA",
+              "Pandas",
+              "SciKit Learn",
+              "Tableau",
+            ]}
+          />
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2008 - 2010"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-        >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Los Angeles, CA
-          </h4>
-          <p>User Experience, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2006 - 2008"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-        >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
-          </h4>
-          <p>User Experience, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="April 2013"
-          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+          contentStyle={elementStyles.contentStyle}
+          contentArrowStyle={elementStyles.arrowStyle}
+          date="2014-2020"
+          iconStyle={elementStyles.workIconStyle}
+          icon={<img src={usyd} alt="usyd" style={elementStyles.imageStyle} />}
         >
           <h3 className="vertical-timeline-element-title">
-            Content Marketing for Web, Mobile and Social Media
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-          <p>Strategy, Social Media</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2002 - 2006"
-          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Bachelor of Science in Interactive Digital Media Visual Imaging
+            Bachelor of Mathematics
           </h3>
           <h4 className="vertical-timeline-element-subtitle">
-            Bachelor Degree
+            University Of Sydney
           </h4>
-          <p>Creative Direction, Visual Design</p>
+          <p>Set Theory, Mathematic Algorithms, Quantum Mechanics, ...</p>
+          <ParentComponent technologies={["MATLAB", "Python"]} />
         </VerticalTimelineElement>
         <VerticalTimelineElement
           iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
